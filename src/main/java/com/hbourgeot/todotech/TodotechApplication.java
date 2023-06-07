65e2922d02c5549a3aadaef619722c5ec4d8e5f8
hbourgeot <henrrybrgt@gmail.com>
2023-06-07 08:26:23 -0400

    first commit

---
+++ src/main/java/com/hbourgeot/todotech/TodotechApplication.java
@@ -0,0 +1,13 @@
+package com.hbourgeot.todotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodotechApplication.class, args);
+	}
+
+}
