a78fcc0d16f58a0e0c31fc4d510eb422811f7f68
hbourgeot <henrrybrgt@gmail.com>
2023-06-29 00:18:15 -0400

    addin comments pt1

--- src/main/java/com/hbourgeot/todotech/controllers/CustomersController.java
+++ src/main/java/com/hbourgeot/todotech/controllers/CustomersController.java
@@ -7,21 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.hbourgeot.todotech.entities.Customers;
 import com.hbourgeot.todotech.services.ICustomerService;
 
+// with requestmapping, we put this controller at localhost:8080/customers
 @Controller
 @RequestMapping(value = "/customers")
 public class CustomersController {
+  //service
   public ICustomerService customerService;
 
+  //constructor
   public CustomersController(ICustomerService cService) {
     this.customerService = cService;
   }
 
+  // methods
+
+  // add customer
   @PostMapping(value = "/add")
   public String saveCustomer(Customers customer) {
     customerService.save(customer);
     return "redirect:/dash/customers";
   }
 
+  //modify customer
   @PutMapping(value = "/modify")
   public String modifyCustomer(Customers customer) {
     customerService.save(customer);
--- src/main/java/com/hbourgeot/todotech/controllers/CustomersRestController.java
+++ src/main/java/com/hbourgeot/todotech/controllers/CustomersRestController.java
@@ -12,17 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/customer")
 public class CustomersRestController {
+    //service
     public ICustomerService customerService;
 
+    //constructor
     public CustomersRestController(ICustomerService cService) {
         this.customerService = cService;
     }
 
+    // methods
+
+    // all customers
     @GetMapping(value = "/all")
     public List<Customers> getCustomers() {
         return customerService.findAll();
     }
 
+    // specific customer
     @GetMapping(value = "/{id}")
     public Customers getCustomerById(@PathVariable Long id) {
         return customerService.findById(id);
