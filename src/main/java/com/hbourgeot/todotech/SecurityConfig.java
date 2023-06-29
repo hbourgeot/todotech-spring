9c6a2d59da793c97afc425fd0a1a4785f7445bbd
hbourgeot <henrrybrgt@gmail.com>
2023-06-29 00:21:55 -0400

    addin comments pt2

--- src/main/java/com/hbourgeot/todotech/SecurityConfig.java
+++ src/main/java/com/hbourgeot/todotech/SecurityConfig.java
@@ -5,25 +5,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+// class for spring security 6 config
+// this is awesome
 @Configuration
 public class SecurityConfig {
 
+    // define a bean
     @Bean
+
+    //define a method of security
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
+            // not protected paths
             auth.requestMatchers("/login").permitAll();
             auth.requestMatchers("/").permitAll();
             auth.requestMatchers("/product/**").permitAll();
             auth.requestMatchers("/swagger-ui/**").permitAll();
             auth.requestMatchers("/static/**").permitAll();
             auth.requestMatchers("/api/**").permitAll();
+
+            // any other request must be authenticated
             auth.anyRequest().authenticated();
         }).oauth2Login(oauth -> {
-            oauth.defaultSuccessUrl("/dash");
+
+            // default success url for oauth after login
+            oauth.defaultSuccessUrl("/dash/products");
+
+            // login page for oauth
             oauth.loginPage("/login");
         }).formLogin(login -> {
             login.disable();
+            // we disable the classic form login
         });
+
+        // return and build
         return http.build();
     }
 
